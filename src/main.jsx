c9ef9ecc31dd8cf716764778d270274a36464a9b
Brittany <bleykam@gmail.com>
2025-10-06 19:27:21 -0400

    clarity

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { Analytics } from "@vercel/analytics/react"
 import App from './App.jsx'
 import './index.scss'
+import Clarity from '@microsoft/clarity';
 
 // Create a root and render the App into the #root div in index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Analytics /> 
+    <Clarity projectId="tm648ektf9" />
   </React.StrictMode>
 )
